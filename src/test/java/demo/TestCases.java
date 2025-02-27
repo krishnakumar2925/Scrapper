e6043c8bca2479dc5b9310b008d661fb23b57b60
krishna <krishnakumar2925@gmail.com>
2025-02-27 20:21:39 +0530

    ME_QA_XSCRAPE_DATA_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,18 +1,20 @@
 package demo;
 
-import org.openqa.selenium.By;
+import java.io.File;
+import java.util.logging.Level;
+
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.logging.Level;
-// import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
+
 
 public class TestCases {
     ChromeDriver driver;
@@ -26,6 +28,37 @@ public class TestCases {
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
      */
+
+     @Test(enabled=true)
+     public void testCase01() throws InterruptedException{
+        Wrappers wrap=new Wrappers(driver);
+        System.out.println("Testcase1: started");
+        //navigate to application
+        wrap.launchApplication("https://www.scrapethissite.com/pages/");
+        //navigate to the page
+        wrap.navigateTo("Hockey Teams: Forms, Searching and Pagination");
+        //store to json
+        wrap.storeWinPercentage();
+        System.out.println("Testcase1: ended");
+     }
+
+     @Test
+     public void testCase02() throws InterruptedException{
+        Wrappers wrap=new Wrappers(driver);
+        System.out.println("Testcase2: started");
+        //navigate to application
+        wrap.launchApplication("https://www.scrapethissite.com/pages/");
+        //navigate to the page
+        wrap.navigateTo("Oscar Winning Films: AJAX and Javascript");
+        //store to json
+        wrap.oscarMovies();
+        File jsonFile=new File("output/oscar-winner-data.json");
+        Assert.assertTrue(jsonFile.exists(),"JSON file does not exist!");
+        Assert.assertTrue(jsonFile.length() > 0, "JSON file is empty!");
+        System.out.println("Testcase2: ended");
+
+
+     }
     @BeforeTest
     public void startBrowser()
     {
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,19 +1,142 @@
 package demo.wrappers;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
+     * 
      */
+    WebDriver driver;
+
+    public Wrappers(WebDriver driver){
+        this.driver=driver;
+        PageFactory.initElements(driver, this);
+    }
+    @FindBy(xpath="//td[contains(@class,'pct')]")
+    List<WebElement> winPercentages;
+
+    
+    @FindBy(xpath="//a[@class='year-link']")
+    List<WebElement> yearText;
+
+    @FindBy(xpath="//tr[@class='film'][position()<=5]")
+    List<WebElement> topMovies;
+
+    By teamsName=By.xpath("./parent::tr//td[contains(@class,'name')]");
+    By years=By.xpath("./parent::tr//td[contains(@class,'year')]");
+    By table=By.xpath("//table[@class='table']");
+
+    public void launchApplication(String Url){
+        driver.get(Url);
+    }
+    public void navigateTo(String text){
+        driver.findElement(By.linkText(text)).click();
+        wait(text);
+
+    }
+    public void wait(String text){
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(3));
+        wait.until(ExpectedConditions.titleContains(text));
+    }
+
+    public void storeWinPercentage() throws InterruptedException{
+        ArrayList<HashMap<String, Object>> teamData=new ArrayList<>();
+        long epochTime = System.currentTimeMillis();
+        for(int i=0;i<3;i++){
+            for(WebElement winPercentage:winPercentages){
+                String winText = winPercentage.getText().trim();
+                if (!winText.isEmpty()) {
+                    try {
+                        double winPercent = Double.parseDouble(winText);
+                        if (winPercent < 0.40) {
+                            HashMap<String, Object> teamInfo = new HashMap<>();
+                            teamInfo.put("Epoch Time of Scrape", epochTime);
+                            String teamName=winPercentage.findElement(teamsName).getText();
+                            String year=winPercentage.findElement(years).getText();
+                            teamInfo.put("Team Name",teamName.trim());
+                            teamInfo.put("Year",year.trim());
+                            teamInfo.put("Win", winPercent);
+                            teamData.add(teamInfo);
+
+                        }
+                    }catch(Exception e){
+                        System.out.println("Skipping invalid percentage: " + winText);
+                    }
+            }
+        }
+        driver.findElement(By.linkText(String.valueOf(i+2))).click();
+        Thread.sleep(3000);
+    }   
+    saveToJson(teamData,"hockey-team-data.json");
 }
+
+public void oscarMovies() throws InterruptedException{
+    ArrayList<HashMap<String, Object>> movieData=new ArrayList<>();
+        long epochTime = System.currentTimeMillis();
+    for(WebElement value:yearText){
+        String currentYear=value.getText();
+        value.click();
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(table));
+        Thread.sleep(1000);
+        for(WebElement movie:topMovies){
+            HashMap<String, Object> movieInfo = new HashMap<>();
+        
+                String title = movie.findElement(By.xpath("./td[1]")).getText().trim();
+                String nomination = movie.findElement(By.xpath("./td[2]")).getText().trim();
+                String awards = movie.findElement(By.xpath("./td[3]")).getText().trim();
+                Boolean isWinner = !movie.findElements(By.xpath("./td[4]/i")).isEmpty(); // Checks if the best picture icon is present
+
+                movieInfo.put("Epoch Time of Scrape", epochTime);
+                movieInfo.put("Year",currentYear.trim());
+                movieInfo.put("Title",title.trim());
+                movieInfo.put("Nomination",nomination.trim());
+                movieInfo.put("Awards",awards.trim());
+                movieInfo.put("isWinner",isWinner);
+                movieData.add(movieInfo);           
+            
+
+        }
+
+        
+    }
+    saveToJson(movieData,"oscar-winner-data.json");
+}
+
+public static void saveToJson(ArrayList<HashMap<String, Object>> data,String path) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        try {
+            if(path.contains("oscar")){
+                File OutputFile = new File("output/"+path);
+                OutputFile.getParentFile().mkdirs();
+                objectMapper.writeValue(OutputFile, data);
+                System.out.println("Data saved to output/"+path);
+            }else{
+                objectMapper.writeValue(new File(path), data);
+                System.out.println("Data saved to "+path);
+            }
+            
+        } catch (IOException e) {
+            System.out.println("Error writing JSON file: " + e.getMessage());
+        }
+    }
+}
+
